836d8b7956fb8702c0aae0c9b3795755239ad015
atulramanjha <atulramanjha@gmail.com>
2020-10-08 01:25:16 +0530

    fix text

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             <img
               className="profile-pic"
               src={profilepic}
-              alt="Tim Baker Profile Pic"
+              alt="Atul Kumar Profile Pic"
             />
           </div>
           <div className="nine columns main-col">
